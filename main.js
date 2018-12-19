a65199beebec938068ded9cf08951a06d739587d
Ethan <me@ethan.link>
2018-12-19 13:07:54 +1100

    Allow basic keyboard shortcuts

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const menubar = require('menubar')
+const Menu = electron.Menu;
 
 var mb = menubar({
     height: 16*1.5*3,
@@ -27,8 +28,29 @@ global.goNormalSize = function() {
 }
 
 mb.on('ready', function ready () {
-  console.log('app is ready')
-  // your app code here
+    console.log('app is ready')
+    // your app code here
+
+    // Allow basic keyboard shortcuts – code from https://pracucci.com/atom-electron-enable-copy-and-paste.html
+    var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 })
 
 mb.on('after-create-window', function ready () {
