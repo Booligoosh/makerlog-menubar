b848872d95d4183ff0e552496370b2a4564dc170
Ethan <me@ethan.link>
2018-12-19 14:00:48 +1100

    Fixed quitting shortcut

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ mb.on('ready', function ready () {
         submenu: [
             { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
             { type: "separator" },
-            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+            { label: "Quit", accelerator: "CmdOrCtrl+Q", click: function() { mb.app.quit(); }}
         ]}, {
         label: "Edit",
         submenu: [
