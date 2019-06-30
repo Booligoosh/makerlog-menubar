3a213bf2827cf20a8b457ee7e4a8fb98d4697750
Ethan <me@ethan.link>
2019-06-30 13:13:47 +1000

    Working on code signing

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ const storage = require('electron-json-storage');
 const indexURL = process.env['MAKERLOG_MENUBAR_CONTEXT'] == 'DEV' ? `file://${__dirname}/index.html` : 'https://makerlog-menubar-app.netlify.com';
 const timePickerURL = process.env['MAKERLOG_MENUBAR_CONTEXT'] == 'DEV' ? `file://${__dirname}/timepicker.html` : 'https://makerlog-menubar-app.netlify.com/timepicker';
 
+require('update-electron-app')() // https://electronjs.org/docs/tutorial/updates#using-updateelectronjsorg
+
 global.fontSize = 16*1.5;
 
 if (process.platform == "darwin") {
